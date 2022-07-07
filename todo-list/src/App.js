8bdd1a13091be2034c9d361c5f86cca0dc8678e2
Shrimp <lcamara0916@gmail.com>
2022-07-07 09:37:11 +0000

    User can now create new todos

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,36 @@
+import { useState, useRef } from 'react';
 import './App.css';
 
 import List from './List';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const todoInput = useRef();
+
+  function createTodo() {
+
+    if (todoInput.current.value === null) return;
+
+    const newTodo = todoInput.current.value;
+
+    const updatedTodos = [...todos, newTodo];
+
+    setTodos(updatedTodos);
+
+    todoInput.current.value = null;
+
+  }
+
   return (
-    <List todos={["Todo 1", "Todo 2", "Todo 3"]}/>
+    <>
+      <div>
+        <input ref={todoInput} className="todo-input" type="text"></input>
+        <button onClick={createTodo}>Create Todo</button>
+      </div>
+      <List todos={todos} />
+    </>
   );
 }
 
